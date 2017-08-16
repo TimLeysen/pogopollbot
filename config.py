c7f0d99cd41a68268ce152160b433bf3fdaefb10
tim <tleysen@gmail.com>
2017-08-16 16:48:39 +0200

    Config for both test version and production version.

--- config.py
+++ config.py
@@ -1,9 +1,15 @@
 test_version = True
 
-# broadcast channel to which to post poll
-output_channel_id = '@PoGoWaaslandRaids'
+# Get chat id: invite bot and type something e.g. /my_id @get_id_bot
+# https://api.telegram.org/bot382260415:AAHcSfajoIkJgvRa93_HraivGqBTjsnwTmE/getUpdates
 
-# group where users can create polls
-input_chat_id = -228155825 # PoGo Waasland Bot
+# bot user names: @PoGoPollBot, @PoGoPollTestBot
 
-bot_token = '427679062:AAHeVxPcKK05S_DvXho4dCM1lu9RHLYbYpg'
+if test_version:
+    output_channel_id = '@PoGoWaaslandTest'
+    input_chat_id = -212819985 # PoGoWaaslandBotTest
+    bot_token = '382260415:AAHcSfajoIkJgvRa93_HraivGqBTjsnwTmE'
+else:
+    output_channel_id = '@PoGoWaaslandRaids'
+    input_chat_id = -228155825 # PoGo Waasland Bot
+    bot_token = '427679062:AAHeVxPcKK05S_DvXho4dCM1lu9RHLYbYpg'
