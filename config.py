18797f0272bf6a3c57aa77b343892d8ef3dc4e16
tim <tleysen@gmail.com>
2017-08-24 21:38:49 +0200

    Get actual chat id. Comparing the chat id needs the actual one (for /raid)

--- config.py
+++ config.py
@@ -4,19 +4,18 @@ test_version = True
 # https://api.telegram.org/bot382260415:AAHcSfajoIkJgvRa93_HraivGqBTjsnwTmE/getUpdates
 
 # bot user names: @PoGoPollBot, @PoGoPollTestBot
+# Use /chatid to get the chat id
 
 if test_version:
     # channel where polls are broadcasted
     output_channel_id = '@PoGoWaaslandRaidsTest'
     # general chat where create, close and delete commands are also posted
-    # output_chat_id = '@PoGoWaaslandTest'
-    output_chat_id = -1001127228363
+    output_chat_id = -1001127228363 # '@PoGoWaaslandTest'
     # chat from where users can send commands to the bot
-    # you can use /chatid to the bot to get the chat id
     input_chat_id = -212819985 # PoGoWaaslandBotTest
     bot_token = '382260415:AAHcSfajoIkJgvRa93_HraivGqBTjsnwTmE'
 else:
     output_channel_id = '@PoGoWaaslandRaids'
-    output_chat_id = '@PoGoWaasland'
+    output_chat_id = -1001127550956 # '@PoGoWaasland'
     input_chat_id = -228155825 # PoGo Waasland Bot
     bot_token = '427679062:AAHeVxPcKK05S_DvXho4dCM1lu9RHLYbYpg'
